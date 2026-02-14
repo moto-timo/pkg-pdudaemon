9020368a3f47d03b92ea3043fa4716bb0bf8a649
Tim Orling <tim.orling@konsulko.com>
2026-02-14 10:34:13 -0800

    New upstream version 1.0.0.24.g61fdb8e

--- pdudaemon/drivers/strategies.py
+++ pdudaemon/drivers/strategies.py
@@ -75,8 +75,11 @@ from pdudaemon.drivers.ipower import LindyIPowerClassic8
 from pdudaemon.drivers.modbustcp import ModBusTCP
 from pdudaemon.drivers.gude1202 import Gude1202
 from pdudaemon.drivers.netio4 import Netio4
+from pdudaemon.drivers.netiojson import NetioJson
 from pdudaemon.drivers.cyberpower81001 import Cyberpower81001
 from pdudaemon.drivers.homeassistant import HomeAssistantHTTP
+from pdudaemon.drivers.ubus import Ubus
+from pdudaemon.drivers.shelly_gen2 import ShellyGen2
 
 __all__ = [
     ACME.__name__,
@@ -134,8 +137,11 @@ __all__ = [
     ModBusTCP.__name__,
     Gude1202.__name__,
     Netio4.__name__,
+    NetioJson.__name__,
     Cyberpower81001.__name__,
     HomeAssistantHTTP.__name__,
+    Ubus.__name__,
+    ShellyGen2.__name__,
 ]
 
 log = logging.getLogger("pdud.drivers." + os.path.basename(__file__))
--- pdudaemon/pdurunner.py
+++ pdudaemon/pdurunner.py
@@ -31,10 +31,11 @@ assert pdudaemon.drivers.strategies, "Subclasses are iterated to find all driver
 
 class PDURunner:
 
-    def __init__(self, config, hostname, retries):
+    def __init__(self, config, hostname, retries, retrydelay):
         self.config = config
         self.hostname = hostname
         self.retries = retries
+        self.retrydelay = retrydelay
         self.logger = logging.getLogger("pdud.pdu.%s" % hostname)
         self.driver = self.driver_from_hostname(hostname)
         # use single-worker ThreadPoolExecutor to serialize execution
@@ -53,13 +54,14 @@ class PDURunner:
         retries = self.retries
         while retries > 0:
             try:
-                return self.driver.handle(request, port)
+                self.driver.handle(request, port)
+                return True
             except (OSError, pexpect.exceptions.EOF, Exception):  # pylint: disable=broad-except
                 self.logger.warn(traceback.format_exc())
                 self.logger.warn("Failed to execute job: {} {} (attempts left {})".format(port, request, retries - 1))
                 if self.driver:
                     self.driver._bombout()  # pylint: disable=W0212,E1101
-                time.sleep(5)
+                time.sleep(self.retrydelay)
                 retries -= 1
                 continue
         return False
